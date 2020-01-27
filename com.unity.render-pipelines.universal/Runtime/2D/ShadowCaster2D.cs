2b69bab6cd068609c4cf46150044fe5a97eecc31
ToastandBananas <37029021+ToastandBananas@users.noreply.github.com>
2020-01-27 10:56:50 -0800

    Add Tilemap Functionality for ShadowCaster2D

--- com.unity.render-pipelines.universal/Runtime/2D/ShadowCaster2D.cs
+++ com.unity.render-pipelines.universal/Runtime/2D/ShadowCaster2D.cs
@@ -81,8 +81,9 @@ namespace UnityEngine.Experimental.Rendering.Universal
         {
             return m_ApplyToSortingLayers != null ? Array.IndexOf(m_ApplyToSortingLayers, layer) >= 0 : false;
         }
-
-        private void Awake()
+        
+        // Original Awake:
+        /*private void Awake()
         {
             if(m_ApplyToSortingLayers == null)
                 m_ApplyToSortingLayers = SetDefaultSortingLayers();
@@ -113,6 +114,48 @@ namespace UnityEngine.Experimental.Rendering.Universal
                     relOffset + new Vector3(-bounds.extents.x, bounds.extents.y)
                 };
             }
+        }*/
+        
+        // New Awake:
+        private void Awake() {
+            if(m_ApplyToSortingLayers == null)
+                m_ApplyToSortingLayers = SetDefaultSortingLayers();
+ 
+            Bounds bounds = new Bounds(transform.position, Vector3.one);
+         
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                bounds = renderer.bounds;
+            }
+            else
+            {
+                Collider2D collider = GetComponent<Collider2D>();
+                if (collider != null)
+                    if (collider.GetType() == typeof(PolygonCollider2D)) {
+                        m_ShapePath = Array.ConvertAll<Vector2, Vector3>(((PolygonCollider2D)collider).GetPath(0), vec2To3);
+                        m_UseRendererSilhouette = false;
+                    } else {
+                        bounds = collider.bounds;
+                    }
+            }
+ 
+            Vector3 relOffset = bounds.center - transform.position;
+ 
+            if (m_ShapePath == null || m_ShapePath.Length == 0)
+            {
+                m_ShapePath = new Vector3[]
+                {
+                    relOffset + new Vector3(-bounds.extents.x, -bounds.extents.y),
+                    relOffset + new Vector3(bounds.extents.x, -bounds.extents.y),
+                    relOffset + new Vector3(bounds.extents.x, bounds.extents.y),
+                    relOffset + new Vector3(-bounds.extents.x, bounds.extents.y)
+                };
+            }
+        }
+
+        private Vector3 vec2To3(Vector2 inputVector) {
+            return new Vector3(inputVector.x, inputVector.y, 0);
         }
 
         protected void OnEnable()
